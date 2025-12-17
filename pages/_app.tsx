2ed97fbfc74200169a8781530c18ccbb758c4861
Christian Vetter <cevaude@pm.me>
2025-12-17 21:14:37 +0100

    feat: Add global PWA meta tags and manifest link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="A sourdough calculator and starter management tool" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="icon" type="image/png" href="/icon-192x192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
